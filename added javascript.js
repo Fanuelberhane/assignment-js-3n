a450011fab7aa62126cd10762fc3f8ae7a33646e
Fanuelberhane <fanuelberhanek@gmail.com>
2024-10-28 10:22:36 +0300

    Add files via upload

---
+++ added javascript.js
@@ -0,0 +1,12 @@
+
+const input=document.getElementById("input");
+const result=document.getElementById("result");
+function addJavaScript() {
+
+    let inputValue=input.value
+
+    result.innerHTML="Javasript "+inputValue
+  
+}
+
+ 
